7f3e28d09ad7e17b8eb96a03b123baee09adabf7
Loren West <loren@evri.com>
2012-06-25 13:24:41 -0700

    Initial setup

---
+++ test/Author.js
@@ -0,0 +1,5 @@
+// Author.js (c) 2012 LucidJS and other contributors
+// Node-monitor may be freely distributed under the MIT license.
+// For further details and documentation:
+// http://lucidjs.github.com/backbone-lifeguard
+
---
+++ test/lifeguard-test.js
@@ -0,0 +1,36 @@
+// lifeguard-test.js (c) 2012 LucidJS and other contributors
+// Node-monitor may be freely distributed under the MIT license.
+// For further details and documentation:
+// http://lucidjs.github.com/backbone-lifeguard
+
+(function(root){
+
+  // Dependencies
+  var Backbone = root.Backbone || require('backbone'),
+      _ = root._ || require('underscore'),
+      Lifeguard = root.BackboneLifeguard || require('../backbone-lifeguard');
+
+  /**
+  * Unit tests for the Backbone-Lifeguard library
+  * @class lifeguard-test
+  */
+
+  /**
+  * Test group for assuring proper Backbone integration
+  *
+  * @method Integration
+  */
+  module.exports['Integration'] = {
+
+    /**
+    * Test the extended toJSON mechanism.
+    * @method Integration-toJSON
+    */
+    toJSON: function(test) {
+      test.ok(true, "Test passed.");
+      test.done();
+    }
+
+  };
+
+}(this));
